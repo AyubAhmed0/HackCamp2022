0bafb6fdac889f5849fe68aa9e94deaaa5ff5ad5
A-dot-A <97310315+A-dot-A@users.noreply.github.com>
2022-01-14 11:48:33 +0000

    LoginAndSignUp
    we just need to add the wrong password or email messages, and on sign up page we need to add registration confirmation.

--- Models/User.php
+++ Models/User.php
@@ -31,21 +31,40 @@ class User
         $statement->execute();
 
     }
-
-    public function fetchUserPassword($email, $password){
-
-
-        $sqlQuery = "SELECT * FROM Users WHERE email = '".$email."'AND password = '".$password."'";
-        $statement = $this->_dbHandle->prepare($sqlQuery);
-
-        $statement->execute();
-        $dataSet = [];
-        while ($row = $statement->fetch()) {
-            $dataSet[] = new UserData($row);
+    public function loginUser($email, $password)
+    {
+        $this->email = trim($email);
+        $this->password = trim($password);
+        $checkEmail = $this->_dbHandle->prepare("SELECT * FROM Users WHERE email = ? ");
+        $checkEmail->execute([$this->email]);
+        $row = $checkEmail->fetch(PDO::FETCH_ASSOC);
+        if($row['email'] == $this->email)
+        {
+            $verifyPass = password_verify($this->password, $row['password']);
+            if($verifyPass)
+            {
+                $_SESSION = [
+                    'user_id' => $row['id'],
+                    'email' => $row['email']
+                ];
+                header('Location: https://www.ajbell.co.uk/');
+            }
+            else
+            {
+                echo '<h1 class="p-3 mb-2 bg-danger text-white"><br>Show password not matched message!<br></h1>';
+            }
         }
-        return $dataSet;
+        else
+        {
+            echo '<h1 class="p-3 mb-2 bg-danger text-white"><br> Show Email is not registered message!<br></h1>';
+        }
+    }
+    public function getSession()
+    {
+        //var_dump($_SESSION);
+        //echo '<h1>Logged in via email:' . $_SESSION['email'];
+        return $_SESSION;
 
     }
-
 
 }
--- index.php
+++ index.php
@@ -5,16 +5,17 @@ require_once('Models/User.php');
 require_once('Views/index.phtml');
 session_start();
 if (isset($_POST["button-login"])) {
+    
     $userEmail = $_POST["email"];
-    //$password = md5($_POST["password"]);
-    $password = ($_POST["password"]);
-    echo $userEmail;
-    echo $password;
-
+    $password = ($_POST["login-password"]);
+    echo "<h1>Email:</h1>";
+    var_dump($userEmail);
+    echo "<h1>Password:</h1>";
+    var_dump($password);
     $login = new User();
-    $loginDataSet = $login->fetchUserPassword($userEmail, $password);
-
-    if ($loginDataSet != null) {
+    //$loginDataSet = $login->fetchUserPassword($userEmail, $password);
+    $login->loginUser($userEmail, $password);
+   /* if ($loginDataSet != null) {
         $_SESSION["login"] = $FirstName;
 //        echo "You are logged in <a href='home.php'>User Page</a>";
         echo $_SESSION['login'];
@@ -22,6 +23,8 @@ if (isset($_POST["button-login"])) {
 
     } else {
         echo "No Match in databse - Error in username and password";
-    }
+    }*/
+    
+
 
 }
